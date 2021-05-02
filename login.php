9dbc230d4e50e0002c205e4a81727f71e5f4e6b6
PardisSatiyari <74048724+PardisSatiyari@users.noreply.github.com>
2021-05-02 12:40:26 +0430

    Login

---
+++ login.php
@@ -0,0 +1,14 @@
+<?php
+  if (isset ($_PSOT['submit'])){
+    $name["admin","pardis", "k2"];
+    $username = $_POST ['username'];
+    $password = $_PST ['password'];
+  }
+
+  if(!$username = &name){
+    echo "<h1>Sorry you are not allowed</h1>";
+  }
+  else{
+    echo "<h1>Welcome</h1>";
+  }
+ ?>
